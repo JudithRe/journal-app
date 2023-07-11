8ea04dac1f4a5aa81805c2a9b9f7d0e1a06345ac
Judith Retzlaff <Judith.Retzlaff@gmx.net>
2023-07-11 16:32:30 +0200

    add use States and local storage for navigation and favouriting

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,34 +1,55 @@
-export default function Form({ children, formTitle, formId }) {
+export default function Form({ formTitle, formId, onNewEntry }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData);
+
+    onNewEntry(data);
+
+    event.target.reset();
+    event.target.motto.focus();
+  }
+
   return (
     <>
       <h2 className="form__title" id={formId}>
         {formTitle}
       </h2>
-      <form className="form" aria-labelledby={formId}>
-        {children}
+      <form onSubmit={handleSubmit} className="form" aria-labelledby={formId}>
+        <FormInput id="motto" inputLabel="Motto" />
+        <FormTextarea id="notes" textareaLabel="Notes" />
+        <button className="button" type="submit">
+          Create
+        </button>
       </form>
     </>
   );
 }
 
-export function FormInput({ id, inputLabel }) {
+function FormInput({ id, inputLabel }) {
   return (
     <>
       <label className="form__label" htmlFor={id}>
         {inputLabel}
       </label>
-      <input className="form__input" name={id} id={id} />
+      <input className="form__input" name={id} id={id} required />
     </>
   );
 }
 
-export function FormTextarea({ id, textareaLabel }) {
+function FormTextarea({ id, textareaLabel }) {
   return (
     <>
       <label className="form__label" htmlFor={id}>
         {textareaLabel}
       </label>
-      <textarea className="form__input" id={id} name={id} rows="4"></textarea>
+      <textarea
+        className="form__input"
+        id={id}
+        name={id}
+        rows="4"
+        required
+      ></textarea>
     </>
   );
 }
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,16 +1,44 @@
-export default function Navigation({ children }) {
-  return <ul class="navigation">{children}</ul>;
-}
-
-export function NavLink({ children, href, state = "" }) {
+export default function Navigation({
+  onShowAllEntries,
+  onShowFavoriteEntries,
+  filter = "all",
+  allCount,
+  favoriteCount,
+}) {
   return (
-    <>
+    <ul class="navigation">
       <li>
-        <a className={`nav__link nav__link--${state}`} href={href}>
-          {children}
-        </a>
-        <span className={`nav__counter nav__counter--${state}`}>3</span>
+        <button
+          className={`nav__link nav__link--${filter === "all" ? "active" : ""}`}
+          onClick={onShowAllEntries}
+        >
+          All Entries
+        </button>
+        <span
+          className={`nav__counter nav__counter--${
+            filter === "all" ? "active" : ""
+          }`}
+        >
+          {allCount}
+        </span>
       </li>
-    </>
+      <li>
+        <button
+          className={`nav__link nav__link--${
+            filter === "favorites" ? "active" : ""
+          }`}
+          onClick={onShowFavoriteEntries}
+        >
+          Favourites
+        </button>
+        <span
+          className={`nav__counter nav__counter--${
+            filter === "favorites" ? "active" : ""
+          }`}
+        >
+          {favoriteCount}
+        </span>
+      </li>
+    </ul>
   );
 }
